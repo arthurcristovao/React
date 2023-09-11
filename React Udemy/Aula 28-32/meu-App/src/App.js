d00592e899613dde074b85c39418ab3347dc7eda
Arthur Cristovão <arthhur122@gmail.com>
2023-09-11 11:51:05 -0300

    Aula 28-32

--- React Udemy/Aula 28-32/meu-App/src/App.js
+++ React Udemy/Aula 28-32/meu-App/src/App.js
@@ -1,8 +1,32 @@
-export default function App(){
-  return (
-    <div>
-      <h1>Bem vindo ao sistema</h1>
-      <h2>Arthur Cristovão</h2>
-    </div>
-  );
-}
+// export default function App(){
+//   return (
+//     <div>
+//       <h1>Bem vindo ao sistema</h1>
+//       <h2>Arthur Cristovão</h2>
+//     </div>
+//   );
+// }
+
+import React from 'react';
+
+  const Bemvindo = (props) => {
+    return(
+      <div>
+        <h1>Bem-vindo(a) {props.nome}!</h1>
+        <h3>Tenho {props.idade} anos!</h3>
+      </div>
+    );
+  }
+
+  function App(){
+    return(
+      <div>
+        Olá, Mundo!
+        <Bemvindo nome="Arthur C" idade="23"/>
+        <Bemvindo nome="João" idade="25"/>
+        <h1>Curso React</h1>
+      </div>
+    );
+  }
+
+export default App;
