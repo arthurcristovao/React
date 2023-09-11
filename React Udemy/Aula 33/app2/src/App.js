c478e8556f981f180e185ec77eb4f49577200984
Arthur Cristovão <arthhur122@gmail.com>
2023-09-11 15:33:24 -0300

    Aula 33

--- React Udemy/Aula 33/app2/src/App.js
+++ React Udemy/Aula 33/app2/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const Equipe = ( props ) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <hr></hr>
+      <Sobre nome = {props.nome} cargo = {props.cargo}
+              idade = {props.idade} />
+      <Social fb={props.facebook}/>
     </div>
   );
 }
+
+const Sobre = (props) => {
+  return(
+    <div>
+      <h2>Olá sou o(a) {props.nome} </h2>
+      <h3>Cargo: {props.cargo} </h3>
+      <h3>Idade: {props.idade} </h3>
+    </div>
+  );
+}
+
+const Social = (props) => {
+  return (
+    <div>
+      <a href={props.fb}> Facebook </a>
+      <a>LinkedIn </a>
+      <a>Youtube </a>
+    </div>
+  );
+}
+
+  function App(){
+    return(
+      <div>
+        <h1>Conheça a nossa equipe: </h1>
+        <Equipe nome="Arthur" cargo="Programador" idade="23" facebook="http://fb.com"/>
+        <Equipe nome="Laura" cargo="Advogada" idade="23" facebook="http://google.com"/>
+      </div>
+    );
+  }
 
 export default App;
